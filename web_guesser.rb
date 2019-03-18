522143c1a41f2002ff87ef46d99320acda48dfdf
Mavioux <pakmarios@gmail.com>
2019-03-19 00:49:50 +0200

    Guessing number calculations

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,10 +2,29 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 number = rand(101)
+guess = "villos"
+message = "I am empty"
 
 
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params['guess']
+  puts guess
 
+  if guess.to_i > number
+    message = "Too high!"
+    puts "I am too high"
+  end
+  
+  erb :index, :locals => {:number => number, :message => message}
+end
+
+
+
+
+
+
+
+get '/' do
+  erb :index, :locals => {:number => number, :message => message}
 end
